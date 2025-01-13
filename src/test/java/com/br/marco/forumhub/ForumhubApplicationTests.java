bbc831fb8a8ec1411ab9d0677b5f1228d7b7702c
Marco Amaral <marco.amaral@gmail.com>
2025-01-13 20:53:42 -0300

    Initial commit

---
+++ src/test/java/com/br/marco/forumhub/ForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.marco.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
